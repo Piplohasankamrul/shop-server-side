45d21fc17e79f15165f8cacb5ff3b269731760e0
piplohasan kamrul <piplohasank@gmail.com>
2022-07-18 18:15:36 +0600

    new

--- index.js
+++ index.js
@@ -58,6 +58,7 @@ async function run() {
     // Add Orders Api
     app.post("/orders", async (req, res) => {
       const order = req.body;
+      order.createdAt = new Date();
       const result = await orderCollection.insertOne(order);
       res.json(result);
     });
